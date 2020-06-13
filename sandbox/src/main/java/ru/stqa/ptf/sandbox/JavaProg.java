380d5415861f14c11927a3e093c0794e8e8851da
Ivan Sidorenko <vanok12@mail.ru>
2020-06-13 18:19:37 +0400

    task2 added

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/JavaProg.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/JavaProg.java
@@ -4,8 +4,18 @@ public class JavaProg {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, world!");
+        Point point = new Point();
+
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(2, 2);
+        Point p3 = new Point(0, 0);
+        Point p4 = new Point(3, 0);
+
+        System.out.println(point.distance(p1, p2));
+        System.out.println(point.distance(p2, p3));
+        System.out.println(point.distance(p4, p3));
 
     }
+
 
 }
