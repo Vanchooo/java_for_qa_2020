b9405dc7e11ffeb5ac511b5a94fc65f4c2fec43e
Ivan Sidorenko <vanok12@mail.ru>
2020-06-17 23:43:17 +0400

    changes in method "distance"

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/JavaProg.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/JavaProg.java
@@ -4,16 +4,15 @@ public class JavaProg {
 
     public static void main(String[] args) {
 
-        Point point = new Point();
+        Point point = new Point(1, 1);
 
-        Point p1 = new Point(1, 1);
         Point p2 = new Point(2, 2);
         Point p3 = new Point(0, 0);
         Point p4 = new Point(3, 0);
 
-        System.out.println(point.distance(p1, p2));
-        System.out.println(point.distance(p2, p3));
-        System.out.println(point.distance(p4, p3));
+        System.out.println(point.distance(p2));
+        System.out.println(point.distance(p3));
+        System.out.println(point.distance(p3));
 
     }
 
--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -6,26 +6,26 @@ import ru.stqa.ptf.sandbox.Point;
 
 public class PointTest {
 
-    Point point = new Point();
+    Point point = new Point(1,1);
 
-    Point p1 = new Point(1, 1);
     Point p2 = new Point(2, 2);
     Point p3 = new Point(0, 0);
     Point p4 = new Point(3, 0);
 
     @Test
     public void testPoint1(){
-        Assert.assertEquals(point.distance(p1,p2),1.4142135623730951);
+        Assert.assertEquals(point.distance(p2),1.4142135623730951);
     }
 
     @Test
     public void testPoint2(){
-        Assert.assertEquals(point.distance(p2, p3),2.8284271247461903);
+        Assert.assertEquals(point.distance(p3),1.4142135623730951);
     }
 
     @Test
     public void testPoint3(){
-        Assert.assertEquals(point.distance(p4, p3),3.0);
+        Assert.assertEquals(point.distance(p3),1.4142135623730951);
     }
+
 
 }
