1b91f51393f5997f6a3d28f12da12ae8b2269741
Ivan Sidorenko <vanok12@mail.ru>
2020-06-22 20:05:33 +0400

    GroupModification test added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,20 +1,15 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-public class GroupCreationTests extends TestBase {
-
+public class GroupModificationTests extends TestBase {
     @Test
-    public void testGroupCreation() throws Exception {
-
+    public void testGroupMofification(){
         app.getNavigationHelper().gotoGroupPage();
-        app.getGroupHelper().initGroupCreation();
+        app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
-        app.getGroupHelper().submitGroupCreation();
+        app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
-
     }
-
-
 }
