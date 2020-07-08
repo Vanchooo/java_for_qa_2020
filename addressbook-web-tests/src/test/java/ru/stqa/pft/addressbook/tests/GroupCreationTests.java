b23bc90891d1c1acf4ff3491ed63f2c7260596b2
Ivan Sidorenko <vanok12@mail.ru>
2020-07-08 17:49:45 +0400

    new assert added in group mod. tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,15 +4,20 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
-        app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before + 1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        GroupData group = new GroupData("test1", null, null);
+        app.getGroupHelper().createGroup(group);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() + 1);
+
+        before.add(group);
     }
 
 }
