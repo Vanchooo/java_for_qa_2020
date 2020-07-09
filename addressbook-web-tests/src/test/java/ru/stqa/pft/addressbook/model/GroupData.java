233545c61e85a91504f70b28ea2d12e7afa5b45b
Ivan Sidorenko <vanok12@mail.ru>
2020-07-09 17:24:40 +0400

    additionals asserts in group creation tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,17 +3,18 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-    private final String id;
+
+    private  int id;
+
     private final String name;
     private final String header;
     private final String footer;
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(id, groupData.id) &&
+        return id == groupData.id &&
                 Objects.equals(name, groupData.name);
     }
 
@@ -30,11 +31,11 @@ public class GroupData {
                 '}';
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    public GroupData(String id, String name, String header, String footer) {
+    public GroupData(int id, String name, String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
@@ -42,10 +43,14 @@ public class GroupData {
     }
 
     public GroupData(String name, String header, String footer) {
-        this.id = null;
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
